2474877c47b88f64a062696153733629afd6bcf7
bruna15limaa <97604487+bruna15limaa@users.noreply.github.com>
2024-02-21 13:15:06 -0300

    register

--- src/pages/register/index.page.tsx
+++ src/pages/register/index.page.tsx
@@ -1,11 +1,28 @@
 import { Button, Heading, MultiStep, Text, TextInput } from '@ignite-ui/react'
 import { ArrowRight } from 'phosphor-react'
-
+import { z } from 'zod'
 import { Container, Form, Header } from './styles'
 import { useForm } from 'react-hook-form'
 
+const registerFormSchema = z.object({
+    username: z
+    .string()
+    .min(3, { message: 'O usuário precisa ter pelo menos 3 letras.' })
+    .regex(/^([a-z\\-]+)$/i, {
+      message: 'O usuário pode ter apenas letras e hifens.',
+    })
+    .transform((username) => username.toLowerCase()),
+  name: z
+    .string()
+    .min(3, { message: 'O nome precisa ter pelo menos 3 letras.' }),
+})
+
 export default function Register() {
-    const { register, handleSubmit, form } = useForm
+    const { 
+        register, 
+        handleSubmit, 
+        formState: { erros, isSubmiting }, 
+    } = useForm()
 
   return (
     <Container>
@@ -30,7 +47,7 @@ export default function Register() {
           <TextInput placeholder="Seu nome" />
         </label>
 
-        <Button type="submit">
+        <Button type="submit" disabled={isSubmiting}>
           Próximo passo
           <ArrowRight />
         </Button>
